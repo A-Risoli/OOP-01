01280d32f2b8eb7be97ed1cb6463d400939533eb
A-Risoli <flicten93@libero.it>
2015-03-11 23:19:41 +0100

    Prima versione

---
+++ OOP1/src/pack1/Contact.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class Contact {
+
+private String nome;
+
+	public Contact(String nome){
+		this.nome=nome;
+	}
+
+	public String get(){
+		return nome;
+						}
+}
---
+++ OOP1/src/pack1/Main.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Contact c=new Contact("ciao");
+		System.out.println(c.get());
+
+	}
+
+}
